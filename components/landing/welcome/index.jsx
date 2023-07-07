83c0a0963610814191553738758c8050bd2ec574
Karan Dalania <karandalania.kd@gmail.com>
2023-07-08 00:18:56 +0530

    image fixed

--- components/landing/welcome/index.jsx
+++ components/landing/welcome/index.jsx
@@ -17,7 +17,7 @@ const Welcome = () => {
                 </div><br/>
                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Shop Now</button>
             </div>
-            <Image src="/comm.jpeg" width={500} height={200} className='ml-auto rounded-r-xl'/>
+            <Image src="/comm.webp" width={500} height={200} className='ml-auto rounded-r-xl'/>
         </div>
         </div>
         </>
