0fc9de6bf46641369f8944ded25674e643d1fb77
Karan Dalania <karandalania.kd@gmail.com>
2023-07-08 00:15:00 +0530

    Api integrated

--- components/landing/cards/index.jsx
+++ components/landing/cards/index.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-
+import Image from 'next/image';
 
 const Cards = () => {
     const [fake,setfake]=useState([]);
@@ -9,86 +9,39 @@ const Cards = () => {
     },[])
     const fakestore = async()=>{
         const response = await fetch ("https://fakestoreapi.com/products");
-    {/*console.log(response);*/}
         const jsondata = await response.json();
-    {/*console.log(jsondata);*/}
     setfake(jsondata);
     }
-{/*fakestore();*/}
     return (
         <>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {Object.keys(fake).map((value)=>{
+        {fake.map((value)=>{
+          console.log(value);
             return(
+              
                 <>
                 <div className='p-10'>
             
-            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="w-full h-[600px] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
      
      <div className="px-5 pb-5">
-       <a href="#">
+       
+       <img class="p-8 rounded-t-lg items-center m-auto justify-center" src={value.image} width='250px'  alt="product image" />
+<div>
          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-           {value}
+           {value.title}
          </h5>
-       </a>
+       
        <div className="flex items-center mt-2.5 mb-5">
-         <svg
-           aria-hidden="true"
-           className="w-5 h-5 text-yellow-300"
-           fill="currentColor"
-           viewBox="0 0 20 20"
-           xmlns="http://www.w3.org/2000/svg"
-         >
-           <title>First star</title>
-           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-         </svg>
-         <svg
-           aria-hidden="true"
-           className="w-5 h-5 text-yellow-300"
-           fill="currentColor"
-           viewBox="0 0 20 20"
-           xmlns="http://www.w3.org/2000/svg"
-         >
-           <title>Second star</title>
-           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-         </svg>
-         <svg
-           aria-hidden="true"
-           className="w-5 h-5 text-yellow-300"
-           fill="currentColor"
-           viewBox="0 0 20 20"
-           xmlns="http://www.w3.org/2000/svg"
-         >
-           <title>Third star</title>
-           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-         </svg>
-         <svg
-           aria-hidden="true"
-           className="w-5 h-5 text-yellow-300"
-           fill="currentColor"
-           viewBox="0 0 20 20"
-           xmlns="http://www.w3.org/2000/svg"
-         >
-           <title>Fourth star</title>
-           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-         </svg>
-         <svg
-           aria-hidden="true"
-           className="w-5 h-5 text-yellow-300"
-           fill="currentColor"
-           viewBox="0 0 20 20"
-           xmlns="http://www.w3.org/2000/svg"
-         >
-           <title>Fifth star</title>
-           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-         </svg>
+         
+         
          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-           5.0
+           {value.rating.rate}
          </span>
        </div>
        <div className="flex items-center justify-between">
          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-           $599
+           ${value.price}
          </span>
          <a
            href="#"
@@ -96,6 +49,7 @@ const Cards = () => {
          >
            Add to cart
          </a>
+       </div>
        </div>
      </div>
    </div>
