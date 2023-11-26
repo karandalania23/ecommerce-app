b631be98166b27d89a70d87f0c58ce35e59789e4
Karan Dalania <karandalania.kd@gmail.com>
2023-11-26 14:59:03 +0530

    Bug fix

--- components/landing/welcome/index.jsx
+++ components/landing/welcome/index.jsx
@@ -4,7 +4,8 @@ const Welcome = () => {
     return (
         <>
             <div class="p-7 ">
-                <div class="bg-[#343434] rounded-xl">
+                <div class="grid grid-cols-1 lg:grid-cols-3 rounded-lg ">
+                    <div class="h-96 bg-[#343434] lg:col-span-2">
                     <div class="absolute text-white p-10 ">
                         <div class="text-3xl font-semibold">
                             Hi There !
@@ -17,7 +18,14 @@ const Welcome = () => {
                         </div><br />
                         <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Shop Now</button>
                     </div>
-                    <Image src="/imgmain.png" width={400} height={200} className='ml-auto p-5 rounded-r-xl' />
+                    </div>
+                    <div class="h-96  bg-[#343434] ">
+                    <Image src="/imgmain.png" width={400} height={200} className='m-auto p-5 rounded-r-xl' />
+                    </div>
+                </div>
+                <div class="bg-[#343434] rounded-xl">
+                    
+                    
                 </div>
             </div>
         </>
